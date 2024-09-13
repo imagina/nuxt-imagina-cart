9ff4a691fab3d6e0d5db051c3a1260558284ae81
WilmerAce <wilmerc@imaginacolombia.com>
2024-09-13 16:15:50 -0500

    main project

---
+++ stores/useAuth.ts
@@ -0,0 +1,59 @@
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        user: null,
+        token: null,
+        expiresIn: null,
+        loading: false,
+    }),
+    getters: {
+        isAuthenticated(state) {
+            if (import.meta.client) {
+                const tokenFromStorage = localStorage.getItem('userToken')
+                return !!tokenFromStorage
+            }
+            return false
+        }
+    },
+    actions: {
+        async login(credentials: {username: string; password: string }): Promise<void> {
+            try {
+                this.loading = true
+                const config = useRuntimeConfig()
+                const response: any = await $fetch(`${config.public.apiRoute}/api/profile/v1/auth/login`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: credentials
+                })
+
+                this.user = response.data.userData
+                this.token = response.data.userToken
+                this.expiresIn = response.data.expiresIn
+
+                localStorage.setItem('userToken', this.token)
+                const router = useRouter()
+                router.push('/home')
+                this.loading = false
+            } catch (error) {
+                console.error('Login failed:', error)
+            }
+        },
+        async logout() {
+            const config = useRuntimeConfig()
+            this.user = null
+            this.token = null
+            this.expiresIn = null
+            localStorage.removeItem('userToken');
+
+            await $fetch(`${config.public.apiRoute}/api/profile/v1/auth/logout`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+        }
+    }
+})
