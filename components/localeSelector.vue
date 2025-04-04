bc4d3a6f68db40b0dd1c3e49a912310d018ab42b
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-04-04 18:29:57 -0500

    + added locale selector component

---
+++ components/localeSelector.vue
@@ -0,0 +1,24 @@
+<template>
+	<q-select 	
+		v-model="localeRef"		
+		:options="locales" 
+		option-label="name"
+		option-value="code"		
+		@update:model-value="setLocale()"
+		outlined		
+		hide-bottom-space
+		dense
+		options-dense		
+		hide-dropdown-icon		
+		emit-value
+	/>
+</template>
+<script setup>
+const { $i18n } = useNuxtApp();
+const { locale, locales } = useI18n()
+const localeRef = ref(locale)
+
+async function setLocale(){
+	await $i18n.setLocale(localeRef.value);
+}
+</script>
