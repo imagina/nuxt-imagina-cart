5487a294587f56eb43ad2fa07aac8b1b03ade3d6
Henry Rodriguez <139818140+hrodriguezimagina@users.noreply.github.com>
2025-04-25 18:10:07 -0500

    Delete .nuxt directory

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///F:/Henry/github/base-nuxt/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///F:/Henry/github/base-nuxt/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
