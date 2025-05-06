09383d8a1234152284846ee9bd0178679cc502ae
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-05-06 15:51:23 -0500

    + validate usd rates

--- modules/icommerce/components/currencySelector/index.vue
+++ modules/icommerce/components/currencySelector/index.vue
@@ -1,6 +1,7 @@
-<template>
+<template>    
 	<q-select
-        :label="$t('icommerce.cart.currency')"
+        v-if="showCurrencies()"
+        :label="$t('icommerce.cart.currency')"        
         class="tw-w-[240px] tw-bg-white"
         v-model="cartState.currency"
         :options="currencies"
@@ -14,11 +15,24 @@
 import { useStorage } from '@vueuse/core'
 import productsHelper from '../../helpers/products'
 
+
 const cartState = useStorage('shoppingCart', {
 	products: [],
 	currency: 'COP'
 })
 
 const currencies = computed(() => productsHelper.getCurrencies())  
+
+function showCurrencies() {
+    const usd = productsHelper.getTrm('COP')
+    if(!usd){
+        cartState.value.currency = 'COP'
+        return false
+    }
+    return true 
+}
+
+
+
 
 </script>
