6f5814c0899e2a08129ded24a4d633b5e43b2d7e
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-05-09 15:44:14 -0500

    + comment unused code

--- plugins/ip.server.ts
+++ plugins/ip.server.ts
@@ -1,8 +1,10 @@
 export default defineNuxtPlugin(() => {
+  /*
     const ip = useState('ip', () => null)
   
     const event = useRequestEvent()
   
     // attempt to get IP
     ip.value = event.req.headers['x-real-ip'] || event.req.headers['x-forwarded-for'] || event.req.socket?.remoteAddress
+    */
   })
