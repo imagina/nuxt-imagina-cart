b8e5923673b67d62acb4667bdce65ab3d18a7ca5
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-05-09 13:34:47 -0500

    + ip lock

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,10 +2,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     const ip = useState('ip', () => null)
     const whiteList = process.env.IP_WHITElIST
     
-    if (!whiteList.includes(ip)) {
+    if (!whiteList.includes(ip.value)) {
         throw createError({
           statusCode: 403,
-          statusMessage: 'Forbidden: Your IP is blocked.',
+          statusMessage: `Forbidden: Your IP is blocked.`,
         });
       }
      
