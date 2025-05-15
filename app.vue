775bca540bae08f40aa0d8630a93f6511654f096
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2025-05-15 13:07:10 -0500

    + added clarity to layout

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+
+import Clarity from '@microsoft/clarity';
+
+const projectId = "rk16tnm12v"
+
+onBeforeMount(() => {
+  Clarity.init(projectId);
+})
+
+
+
 useSeoMeta({
   title: 'Páginas Web - Hosting y Registro de Dominios',
   description: 'Lideres en Hosting y registro de Dominios en Colombia. Mayor rendimiento al mejor precio. 15 años de experiencia más de 10.000 clientes confían en nosotros. Asesores en Bogotá, Ibagué  y Medellín.',  
